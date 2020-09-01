358af4dcf483ca16db382b88d2d51d4bd5dc9368
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-01 00:10:37 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,10 @@
 # Write your code here
+countdown_number = 10
+while countdown_number > 0 do 
+  puts "#{countdown_number}"
+  countdown_number = countdown_number - 1
+end
+
+while true do
+  puts "Happy New Year!"
+end
